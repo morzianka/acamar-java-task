8d616ea1560887b2d31b462e36977ac5c5504e4f
an <pistazie@gmx.com>
2022-10-10 11:26:55 +0300

    master tets

--- src/main/java/cz/acamar/tasks/Task1.java
+++ src/main/java/cz/acamar/tasks/Task1.java
@@ -17,6 +17,6 @@ public class Task1 {
      * @return - the length of the last word in the string.
      */
     public int lengthOfLastWord(String str) {
-        return 0;
+        return -1;
     }
 }
