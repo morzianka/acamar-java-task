d16d16e554b7d22c1962722aeeb86b0de9138ed4
an <pistazie@gmx.com>
2022-10-10 12:54:32 +0300

    master task1,2

--- src/main/java/cz/acamar/tasks/Task1.java
+++ src/main/java/cz/acamar/tasks/Task1.java
@@ -17,6 +17,7 @@ public class Task1 {
      * @return - the length of the last word in the string.
      */
     public int lengthOfLastWord(String str) {
-        return -1;
+        String[] arr = str.split(" ");
+        return arr[arr.length - 1].length();
     }
 }
